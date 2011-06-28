ec6e54c05008fd70da529330b4988536f4163a34
Tim O'Brien <tobrien@discursive.com>
2011-06-27 21:21:16 -0500

    Removing some directories from the workout example

--- workout-site/src/main/webapp/js/cufon-replace.js
+++
@@ -1,3 +0,0 @@
-Cufon.replace('#header ul li a', { fontFamily: 'Myriad Pro i', hover:true });
-Cufon.replace('h2', { fontFamily: 'Myriad Pro si' })
-Cufon.replace('h3', { fontFamily: 'Myriad Pro bl' })
